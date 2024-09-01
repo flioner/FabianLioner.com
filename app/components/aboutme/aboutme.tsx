10d3f45bb312d108515ceed54c19a555f9e5ac41
Lioner Rocha <lionerrochae@gmail.com>
2024-09-01 03:45:58 -0600

    build fix

--- app/components/aboutme/aboutme.tsx
+++ app/components/aboutme/aboutme.tsx
@@ -2,6 +2,20 @@
 import s from "./aboutme.module.css";
 import React, { useState } from "react";
 
+const text = `
+  As a front-end developer specializing in React and Next.js, I bring
+  a strong blend of technical expertise and creative flair to my work.
+  With experience refactoring and leading the front-end development of
+  national-level tools, I excel in architecture, frameworks, and UI/UX
+  design. My journey began as an intern at Tec de Monterrey, where I
+  honed my skills in web development, and I'm now focusing on AI and
+  machine learning. Proficient in CSS animations and passionate about
+  exploring new technologies like Three.js, I strive to create
+  refined, impactful solutions that push the boundaries of innovation.
+  Soon to graduate in Computer Science with a focus on AI, I am
+  committed to delivering excellence in every project I undertake.
+`;
+
 export default function AboutMe() {
   return (
     <main className={s.mainBody}>
@@ -10,17 +24,7 @@ export default function AboutMe() {
         <div className={s.descr}>
           <div className={s.text}>
             <div className={s.title}>About Me</div>
-            As a front-end developer specializing in React and Next.js, I bring
-            a strong blend of technical expertise and creative flair to my work.
-            With experience refactoring and leading the front-end development of
-            national-level tools, I excel in architecture, frameworks, and UI/UX
-            design. My journey began as an intern at Tec de Monterrey, where I
-            honed my skills in web development, and I'm now focusing on AI and
-            machine learning. Proficient in CSS animations and passionate about
-            exploring new technologies like Three.js, I strive to create
-            refined, impactful solutions that push the boundaries of innovation.
-            Soon to graduate in Computer Science with a focus on AI, I am
-            committed to delivering excellence in every project I undertake.
+            {text}
           </div>
         </div>
       </div>
